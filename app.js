3915a6261efcdf1617ce7fb17d6baaefa393296b
Taka <user@USERnoMacBook-Pro.local>
2022-11-02 01:39:06 +0900

    faviconの設定を追加

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const PORT = process.env.PORT;
 const express = require("express");
 const path = require("path");
+const favicon = require("serve-favicon");
 
 const app = express();
+// Express setting（無くても動作する）
+app.set("view engine", "ejs");
 
 // 静的コンテンツのルーティング
+app.use(favicon(path.join(__dirname, "/public/favicon.ico")));
 app.use("/public", express.static(path.join(__dirname, "/public")));
 
 // 動的コンテンツのルーティング
