6ae6a3bd92e61c850f15e71cb84891493c8cf3ae
Taka <user@USERnoMacBook-Pro.local>
2022-11-02 01:03:44 +0900

    静的コンテンツのルーティング

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const PORT = process.env.PORT;
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
 app.get("/", (req,res) => {
   res.end("Hello World");
 });
+
+// 静的コンテンツのルーティング
+app.use("/public", express.static(path.join(__dirname, "/public")));
+
 
 app.listen(PORT, () => {
   console.log("open server");
