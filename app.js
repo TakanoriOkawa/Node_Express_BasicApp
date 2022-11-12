241a985fbb695585e1c451634e59d69c9547661c
Taka <user@USERnoMacBook-Pro.local>
2022-11-13 00:30:52 +0900

    x-poweredの隠蔽

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const favicon = require("serve-favicon");
 const app = express();
 // Express setting（無くても動作する）
 app.set("view engine", "ejs");
+app.disable("x-powered-by");
 
 // 静的コンテンツのルーティング
 app.use(favicon(path.join(__dirname, "/public/favicon.ico")));
