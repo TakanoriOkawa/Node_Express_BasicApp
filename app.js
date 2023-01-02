99c1b16a6eba62f45dd57778d0a73c14f3f546d1
Taka <okawatakanori9916@gmail.com>
2023-01-03 02:23:46 +0900

    ejsテンプレート内で実行できる独自関数の実装

--- app.js
+++ app.js
@@ -11,6 +11,14 @@ const app = express();
 app.set("view engine", "ejs");
 app.disable("x-powered-by");
 
+// Expose global method to view engine
+app.use((req,res,next) => {
+  // ここで渡しているのがレスポンスのオブジェクトに追加して渡している
+  res.locals.moment = require("moment");
+  res.locals.padding = require("./lib/math.js").padding;
+  next();
+});
+
 // Static resource rooting.
 app.use(favicon(path.join(__dirname, "/public/favicon.ico")));
 app.use("/public", express.static(path.join(__dirname, "/public")));
