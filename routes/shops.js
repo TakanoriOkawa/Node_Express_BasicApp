23c44010a98b2cba9012b642ab0f1ef97ca50302
Taka <okawatakanori9916@gmail.com>
2023-01-06 22:03:05 +0900

    店舗情報のルーティングを実装

---
+++ routes/shops.js
@@ -0,0 +1,22 @@
+const router = require("express").Router();
+const { MySQLClient, sql } = require("../lib/database/client.js");
+
+// SQLでの取得処理があるのでasyncをつける
+router.get("/:id", async (req,res,next) => {
+  const id = req.params.id;
+
+  // 後々口コミ情報（_t_review？）他のテーブルからの取得処理と結合するので、Promise.allを使う。
+  Promise.all([
+    MySQLClient.executeQuery(
+      await sql("SELECT_SHOP_DETAIL_BY_ID"),
+      [id] // お店のID
+    )
+  ]).then(result => {
+    const data = result[0][0];
+    res.render("./shops/index.ejs", data); // テンプレートへデータを送る
+  }).catch(err => {
+    next(err); // ログ出力へエラー内容を送る
+  });
+});
+
+module.exports = router;
